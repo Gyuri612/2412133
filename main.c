bec172797fcdaf6c607f1fa523b632ebd439b5ba
Gyuri612 <gyuri6581@sookmyung.ac.kr>
2024-09-11 15:43:01 +0900

    week2
    수정

--- main.c
+++ main.c
@@ -5,8 +5,12 @@
 
 int main(int argc, char *argv[]) {
 	
-	printf("Hello, Sookmyung Electronics!");
-
+	int a, b;
+	a=10; 
+	b=20;
+	
+	printf("Hello, Sookmyung Electronics!\n");
+	printf("%d+%d=%d\n",a,b,a+b);
 	return 0;
 
 }
