202f28377d5c57379f04ebdd986486e2904d2cf6
Gyuri612 <gyuri6581@sookmyung.ac.kr>
2024-11-26 16:11:26 +0900

    week12

--- week12/week12-4/main.c
+++ week12/week12-4/main.c
@@ -4,13 +4,24 @@ int main(void)
 {
 	int i;
 	int grade[5];
+	int sum=0;
+	int *p=grade; 
 	
 	for(i=0;i<5;i++)
 	{
-		printf("Input value - grade[%i]=",i);
-		scanf("%d",&grade[i]);
+		printf("Input value - grade[%i] = ",i);
+		scanf("%d", &grade[i]);
 	}
 	
+	for(i=0;i<5;i++)
+	{
+		printf("grade[%i] = %i\n", i, *(p+i));
+		sum += *(p+i);
+	}
+	
+	printf("Average: %i", sum/5);
+	
+ 
 	
 	return 0;
 }
